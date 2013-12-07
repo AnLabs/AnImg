debcd85c498fcaad19c22e40549bad5240b8043c
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-07 19:45:10 +0100

    Fixing font bruting, and making things scalable

--- src/AnLabs/AnImg/Badge.php
+++ src/AnLabs/AnImg/Badge.php
@@ -11,55 +11,56 @@ final class Badge
 {
     const FONT_PATH_WINDOWS = "C:\\Windows\\Fonts\\";
 
-    public function createGitHubBadge($leftText, $rightText, Lib\Color $color = null)
+    public function createGitHubBadge($leftText, $rightText, Lib\Color $color = null, $fontSize = 11)
     {
         if (!$color) {
             $color = Lib\Color::turquoise();
         }
 
+        $marginFactor = $fontSize / 10;
+        $roundRadius = 2 * $marginFactor;
         $margin = [
-            "top"    => 4,
-            "bottom" => 4,
-            "left"   => 4,
-            "right"  => 4,
+            "top"    => 4 * $marginFactor,
+            "bottom" => 4 * $marginFactor,
+            "left"   => 4 * $marginFactor,
+            "right"  => 4 * $marginFactor,
         ];
         $text = $leftText . $rightText;
 
         // Currently Windows only :(
-        $font = $this->bruteFontSize(self::FONT_PATH_WINDOWS . "DejaVuSansCondensed.ttf", $text, 11);
+        $font = $this->bruteFontSize(self::FONT_PATH_WINDOWS . "DejaVuSansCondensed.ttf", $text, $fontSize);
         $width = [
             "left"  => $font->width($leftText),
             "right" => $font->width($rightText),
             "total" => $font->width($text),
         ];
-        $badgeWidth = 4 * $margin["left"] + 4 * $margin["right"] + $width["total"];
-        $badgeHeight = $margin["top"] + $margin["bottom"] + $font->height($text);
+        $badgeWidth = (int)(4 * $margin["left"] + 4 * $margin["right"] + $width["total"]);
+        $badgeHeight = (int)($margin["top"] + $margin["bottom"] + $font->height($text));
 
         $badgeImage = new Lib\TCGDImage($badgeWidth, $badgeHeight);
         $badgeImage->fillColor(255, 255, 255, 0);
 
         $leftSideWidth = $margin["left"] * 4 + $width["left"];
-        $badgeImage->roundedRectangle(0, 0, $leftSideWidth + 5, $badgeHeight - 1, Lib\Color::grey());
-        $badgeImage->roundedRectangle($leftSideWidth, 0, $badgeWidth - 1, $badgeHeight - 1, $color);
+        $badgeImage->roundedRectangle(0, 0, $leftSideWidth + 5, $badgeHeight - 1, Lib\Color::grey(), $roundRadius);
+        $badgeImage->roundedRectangle($leftSideWidth, 0, $badgeWidth - 1, $badgeHeight - 1, $color, $roundRadius);
 
         $text_color = $badgeImage->allocateColor(255, 255, 255);
         
         $textY = $margin["top"] + $font->height($text) - 2;
-        $this->createShadowText($badgeImage, $leftText, 2 * $margin["left"], $textY, $text_color, $font);
+        $this->createShadowText($badgeImage, $leftText, 2 * $margin["left"], $textY, $text_color, $font, $marginFactor / 2);
 
         $leftDistance = $margin["left"] * 3 + $margin["right"] * 3 + $width["left"];
-        $this->createShadowText($badgeImage, $rightText, $leftDistance, $textY, $text_color, $font);
+        $this->createShadowText($badgeImage, $rightText, $leftDistance, $textY, $text_color, $font, $marginFactor / 2);
 
         return $badgeImage;
     }
 
-    private function createShadowText(Lib\TCGDImage $image, $text, $x, $y, Lib\ImageColor $color, Lib\ImageFont $font)
+    private function createShadowText(Lib\TCGDImage $image, $text, $x, $y, Lib\ImageColor $color, Lib\ImageFont $font, $offset = 1)
     {
         $oldRgb = $color->rgbColor();
         $shadeColor = $image->allocateColor($oldRgb[0] * 0.3, $oldRgb[1] * 0.3, $oldRgb[2] * 0.3);
         $textTransformer = new TextTransformer;
-
-        $offset = 1;
+        $offset = max([1, $offset]);
 
         $textTransformer->applyText($image, $text, $x + $offset, $y + $offset, $shadeColor, $font);
         $textTransformer->applyText($image, $text, $x, $y, $color, $font);
@@ -69,11 +70,14 @@ final class Badge
     {
         $start = 1;
         $newFont = new Lib\ImageFont($font, $start);
-        $height = $newFont->height($text);
-        while ((int)$height != (int)$wanted) {
+        $height = (int)$newFont->height($text);
+        $oldHeight = $height;
+
+        while (!($oldHeight < $wanted && $wanted < $height || $wanted == $height)) {
             $start += 1;
             $newFont = new Lib\ImageFont($font, $start);
-            $height = $newFont->height($text);
+            $oldHeight = $height;
+            $height = (int)$newFont->height($text);
         }
 
         return $newFont;
